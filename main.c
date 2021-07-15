2c6a79e01755a5b3a2d353ce033316340d2afda1
Paulo Santana <paulo.santana.r@gmail.com>
2021-07-15 20:58:08 -0300

    add function to multiply

--- main.c
+++ main.c
@@ -4,10 +4,15 @@ int sum(int x, int y) {
 	return x + y;
 }
 
+int mul(int x, int y) {
+	return x * y;
+}
+
 int main(void) {
-	int x = 1;
-	int y = 2;
+	int x = 5;
+	int y = 3;
 	printf("%i\n", sum(x, y));
+	printf("%i\n", mul(x, y));
 	return (0);
 }
 
